ca90c07c8d7f2500ee480df2d100f32632828e87
COPART\disoni <dishant.soni@copart.com>
2017-01-07 12:41:55 -0600

    basic sayHi function

---
+++ index.js
@@ -0,0 +1,10 @@
+/*** Hello World myGet Node Module
+ * Created to test node module publishing 
+ * on myGet repository ***/
+
+
+module.exports = {
+	sayHello : function(name) {
+		return "Hello , " + name;
+	}
+}
